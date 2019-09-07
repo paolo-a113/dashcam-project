59954989c689835eb2899fe8bbf1553a36049346
paolo-a113 <pargu113@gmail.com>
2019-09-06 23:54:59 -0700

    added stuff

--- record_video.py
+++ record_video.py
@@ -19,21 +19,62 @@ out = cv2.VideoWriter('./vids/outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G')
 while(True):
   ret, frame = cap.read()
 
-  if ret == True:
+	if ret == True:
+		# grab the raw NumPy array representing the image, then initialize the timestamp
+		# and occupied/unoccupied text
+		frame = frame.array
 
-    # Write the frame into the file 'output.avi'
-    out.write(frame)
+		#analyze difference
+		if Start == 1:
+			og_frame = np.copy(frame)
+			Start = 0
 
-    # Display the resulting frame
-    cv2.imshow('frame',frame)
+		og_frame_gray = cv2.cvtColor(og_frame, cv2.COLOR_BGR2GRAY)
+		frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		diff_frame = cv2.absdiff(og_frame_gray, frame_gray)
 
-    # Press Q on keyboard to stop recording
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-      break
+		if np.mean(diff_frame) > mThresh:
+			isMotion = 1
+			print("MOTION")
+			mEventStart = time.time()
+			mNow = datetime.datetime.now()
+			mNow = mNow.strftime("%Y-%m-%d_%H-%M-%S")
+
+			if mEvent == 0:
+				print("START RECORDING")
+				mEvent = 1
+				rec = cv2.VideoWriter('./vids/'+mNow+'.avi',fourcc,5, size)
+				rec.write(frame)
+				mNowStart = mNow
+
+		else:
+			isMotion = 0
+			print("")
+
+			if mEvent == 1:
+				if (time.time() - mEventStart) < 10:
+					rec.write(frame)
+					print(time.time()-mEventStart)
+
+				else:
+					print("STOP RECORDING")
+					rec.release()
+					mEvent = 0
+					os.system("ffmpeg -i ./vids/" + mNowStart + ".avi ./vids/"+ mNowStart + ".mp4")
+					os.system("rm ./vids/" + mNowStart + ".avi")
+
+
+
+		# Display the resulting frame
+		cv2.imshow('frame',frame)
+
+		# Press Q on keyboard to stop recording
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+		  break
 
   # Break the loop
-  else:
-    break
+	else:
+		break
 
 # When everything done, release the video capture and video write objects
 cap.release()
